b0734fcd7063ac432f31be637072804fce00f606
kimgeon5023 <267482197+kimgeon5023@users.noreply.github.com>
2026-09-08 14:29:57 +0900

    Update Android app name and launcher icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.kimgeon.where',
-  appName: 'Where',
+  appName: '갈래말래',
   webDir: 'dist'
 };
 
